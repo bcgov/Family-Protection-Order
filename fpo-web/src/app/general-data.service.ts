5ae021f36c2858133fc05e06372138afd75cb564
Andrew Whitehead <cywolf@gmail.com>
2020-02-07 14:00:45 -0800

    remove debug override
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- fpo-web/src/app/general-data.service.ts
+++ fpo-web/src/app/general-data.service.ts
@@ -27,8 +27,7 @@ export class GeneralDataService {
   }
 
   getApiUrl(action: string): string {
-    return "http://localhost:8081/api/v1/" + action;
-    // return this.getBaseHref() + 'api/' + action;
+    return this.getBaseHref() + 'api/' + action;
   }
 
   getBrowserUser() {
